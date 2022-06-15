acd05627322f689f9501412c1204368f2155e3f1
Diego Moreira <diego.moreira529@gmail.com>
2022-06-14 21:29:29 -0300

    Completed exercises 1 to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array) {
+  //   let newArray = array.filter((item) => item !== toBeRemoved);
+  for (let i = 1; i < arguments.length; i++) {
+    array = array.filter((item) => item !== arguments[i]);
+  }
+  return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
