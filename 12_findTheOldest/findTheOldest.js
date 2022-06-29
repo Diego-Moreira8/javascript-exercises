501f1e464afd0e428243072eee2ce0a22379df7a
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2022-06-29 11:28:09 -0300

    completed 11 and half of 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let sorted = people.sort((a, b) => {
+    a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? 1 : -1;
+  });
+  for (let i = sorted.length - 1; i >= 0; i--) {
+    if (sorted[i].hasOwnProperty("yearOfDeath")) return sorted[i];
+  }
 };
-
+// falta o último teste
 // Do not edit below this line
 module.exports = findTheOldest;
