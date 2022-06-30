10d4a895c0ceacadc3cb62015a7ad916e6e36111
Diego Moreira <diego.moreira529@gmail.com>
2022-06-29 21:26:48 -0300

    Completed 3 tests on ex13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,36 @@
-const caesar = function() {
-
+const caesar = function (string, shiftFactor) {
+  let newString = [];
+  for (let letter of string) {
+    if (letter.match(/[a-z]/gi)) {
+      if (letter.match(/[A-Z]/)) {
+        if (letter.charCodeAt(0) + shiftFactor > 90) {
+          newString.push(
+            String.fromCharCode(letter.charCodeAt(0) - 90 + shiftFactor + 64)
+          );
+        } else {
+          newString.push(
+            String.fromCharCode(letter.charCodeAt(0) + shiftFactor)
+          );
+        }
+      } else if (letter.match(/[a-z]/)) {
+        if (letter.charCodeAt(0) + shiftFactor > 122) {
+          newString.push(
+            String.fromCharCode(letter.charCodeAt(0) - 90 + shiftFactor + 64)
+          );
+        } else {
+          newString.push(
+            String.fromCharCode(letter.charCodeAt(0) + shiftFactor)
+          );
+        }
+      }
+    } else {
+      newString.push(letter);
+    }
+  }
+  return newString.join("");
 };
+
+console.log(caesar("Hello, World!", 5));
 
 // Do not edit below this line
 module.exports = caesar;
