dcf3d99f12ee68cbf8d64e33063643d8bf4cd2cf
Diego Moreira <diego.moreira529@gmail.com>
2022-06-30 21:49:55 -0300

    Trying another approach on 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,36 +1,28 @@
 const caesar = function (string, shiftFactor) {
-  let newString = [];
+  const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+  let newString = "";
   for (let letter of string) {
-    if (letter.match(/[a-z]/gi)) {
-      if (letter.match(/[A-Z]/)) {
-        if (letter.charCodeAt(0) + shiftFactor > 90) {
-          newString.push(
-            String.fromCharCode(letter.charCodeAt(0) - 90 + shiftFactor + 64)
-          );
-        } else {
-          newString.push(
-            String.fromCharCode(letter.charCodeAt(0) + shiftFactor)
-          );
+    if (letter.match(/[a-z]/gi)) { // is it letter?
+      if (letter.match(/[A-Z]/)) { // is it uppercase?
+        if (upperAlphabet.indexOf(letter) + shiftFactor > 26) {
+          newString += upperAlphabet[upperAlphabet.indexOf(letter) + (shiftFactor % 26)]
         }
-      } else if (letter.match(/[a-z]/)) {
-        if (letter.charCodeAt(0) + shiftFactor > 122) {
-          newString.push(
-            String.fromCharCode(letter.charCodeAt(0) - 90 + shiftFactor + 64)
-          );
-        } else {
-          newString.push(
-            String.fromCharCode(letter.charCodeAt(0) + shiftFactor)
-          );
+        newString += upperAlphabet[upperAlphabet.indexOf(letter) + shiftFactor]
+      } else if (letter.match(/[a-z]/)) { // is it lowercase?
+        if (lowerAlphabet.indexOf(letter) + shiftFactor > 26) {
+          newString += upperAlphabet[upperAlphabet.indexOf(letter) + (shiftFactor % 26)]
         }
+        newString += lowerAlphabet[lowerAlphabet.indexOf(letter) + shiftFactor]
       }
     } else {
-      newString.push(letter);
+      newString += letter
     }
   }
-  return newString.join("");
+  return newString
 };
 
-console.log(caesar("Hello, World!", 5));
+console.log(caesar("zzz", 1));
 
 // Do not edit below this line
 module.exports = caesar;
